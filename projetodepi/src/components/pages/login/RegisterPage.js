e99b75dee5407c968254beae532b577e07f69403
mariajaps <166346577+mariajaps@users.noreply.github.com>
2024-06-06 18:24:04 +0000

    cadastro

--- projetodepi/src/components/pages/login/RegisterPage.js
+++ projetodepi/src/components/pages/login/RegisterPage.js
@@ -1,10 +1,56 @@
 //página de cadastro
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './RegisterPage.css'; // Importe os estilos necessários
 
-function RegisterPage (){
-    return(
-        <div>
-            <p>cadastro</p>
+function RegisterPage() {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Valide os campos de entrada aqui
+        if (email.trim() === '' || username.trim() === '' || password.trim() === '' || confirmPassword.trim() === '') {
+            setError('Por favor, preencha todos os campos.');
+            return;
+        }
+        if (password !== confirmPassword) {
+            setError('As senhas não correspondem.');
+            return;
+        }
+        // Se tudo estiver correto, você pode prosseguir com a próxima etapa do cadastro
+        navigate('/NextStep');
+    };
+
+    return (
+        <div className="register-page">
+            <h2>Cadastro</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Email:</label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Nome de Usuário:</label>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Criar Senha:</label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Repetir Senha:</label>
+                    <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                </div>
+                {error && <p className="error-message">{error}</p>}
+                <button type="submit">Próximo</button>
+            </form>
         </div>
-    )
+    );
 }
+
 export default RegisterPage;
