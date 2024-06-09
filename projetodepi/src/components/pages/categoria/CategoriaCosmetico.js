e306b7de7a8ba8db4398291b1da94b977a5f6e9a
Beatriz862 <130605338+Beatriz862@users.noreply.github.com>
2024-06-09 01:55:57 -0300

    Update CategoriaCosmetico.js

--- projetodepi/src/components/pages/categoria/CategoriaCosmetico.js
+++ projetodepi/src/components/pages/categoria/CategoriaCosmetico.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './Categoria.css';
 
 const products = [
-    { id: 1, name: 'Vestido estampado Viscose', price: 'R$379', image: 'https://via.placeholder.com/150' },
-    { id: 2, name: 'Camisata CLASSIC Algodão', price: 'R$147', image: 'https://via.placeholder.com/150' },
-    { id: 3, name: 'Camisa de linho de manga longa', price: 'R$83,90', image: 'https://via.placeholder.com/150' },
-    { id: 4, name: 'Vestido em Liocel Azul', price: 'R$159,90', image: 'https://via.placeholder.com/150' },
-    { id: 5, name: 'Vestido preto tubinho canelado Poliamida', price: 'R$85,90', image: 'https://via.placeholder.com/150' },
-    { id: 6, name: 'Jaqueta Retro Poliéster reciclado', price: 'R$1.542', image: 'https://via.placeholder.com/150' },
+  { id: 1, name: 'Batom CC hidratante', price: 'R$49,90', image: 'https://brunatavares.vtexassets.com/arquivos/ids/158435-1600-auto?v=638106840594800000&width=1600&height=auto&aspect=true/100' },
+  { id: 2, name: 'Base sérum', price: 'R$147', image: 'https://www.marimariamakeup.com/arquivos/ids/164964-undefined-undefined/Base-e-corretivo-matte-velvet-skin-caramelo-mari-maria-make-up.png?v=638457706470500000/100' },
+  { id: 3, name: 'Blush color face', price: 'R$83,90', image: 'https://brunatavares.vtexassets.com/arquivos/ids/158018-1600-auto?v=638035627470730000&width=1600&height=auto&aspect=true/100' },
+  { id: 4, name: 'Perfume infusão botanica 75ml', price: 'R$259,90', image: 'https://www.granado.com.br/media/catalog/product/e/a/ean7896512959218_2247_3.jpg?optimize=high&fit=bounds&height=1614&width=1254&canvas=1254:1614/100' },
+  { id: 5, name: 'Corretivo', price: 'R$149,90', image: 'https://brunatavares.vtexassets.com/arquivos/ids/158192-1600-auto?v=638060243340800000&width=1600&height=auto&aspect=true/100' },
+  { id: 6, name: 'Esfoliante para lábios', price: 'R$65,90', image: 'https://brunatavares.vtexassets.com/arquivos/ids/157164-1600-auto?v=638017498826230000&width=1600&height=auto&aspect=true/100' },,
   ];
   
 function CategoriaCosmetico (){
