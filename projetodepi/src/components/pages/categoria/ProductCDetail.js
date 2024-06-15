30e1bad5f275b118d5efdbe36c5f957b5bcceade
Trikest <130517609+Trikest@users.noreply.github.com>
2024-06-15 00:29:22 -0300

    Update ProductCDetail.js
    Url

--- projetodepi/src/components/pages/categoria/ProductCDetail.js
+++ projetodepi/src/components/pages/categoria/ProductCDetail.js
@@ -14,7 +14,7 @@ function ProductCDetail() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/productCOS")
+    fetch("https://pi-3-concon.onrender.com/productCOS")
       .then(response => response.json())
       .then(data => setProducts(data))
       .catch(error => console.error('Error fetching data:', error));
