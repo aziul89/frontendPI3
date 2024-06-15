ef01244175f12dab36053ef541c81cb17eb4fa17
Trikest <130517609+Trikest@users.noreply.github.com>
2024-06-15 00:32:52 -0300

    Update CategoriaAcessorio.js
    Final

--- projetodepi/src/components/pages/categoria/CategoriaAcessorio.js
+++ projetodepi/src/components/pages/categoria/CategoriaAcessorio.js
@@ -6,7 +6,7 @@ function CategoriaRoupa (){
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/productAC")
+    fetch("https://pi-3-concon.onrender.com/productAC")
       .then(response => response.json())
       .then(data => setProducts(data))
       .catch(error => console.error('Error fetching data:', error));
