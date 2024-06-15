a70101c638c69d367aa72fc3a1900ed15cb23ffc
Trikest <130517609+Trikest@users.noreply.github.com>
2024-06-15 00:31:45 -0300

    Update CategoriaCosmetico.js
    Teste

--- projetodepi/src/components/pages/categoria/CategoriaCosmetico.js
+++ projetodepi/src/components/pages/categoria/CategoriaCosmetico.js
@@ -6,7 +6,7 @@ function CategoriaCosmetico() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/productCOS")
+    fetch("https://pi-3-concon.onrender.com/productCOS")
       .then(response => response.json())
       .then(data => setProducts(data))
       .catch(error => console.error('Error fetching data:', error));
